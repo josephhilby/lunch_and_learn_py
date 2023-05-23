db55cb4a742c14fd7094d79308d6094b2be24b84
Joseph Hilby <joseph.hilby@gmail.com>
2023-05-23 15:14:01 -0500

    Fix error in project url file

--- lunch_and_learn_be/urls.py
+++ lunch_and_learn_be/urls.py
@@ -15,15 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework import routers
-from lunch_and_learn_api import views
-
-router = routers.DefaultRouter()
-router.register(r'users', views.UserViewSet)
-router.register(r'favorites', views.FavoriteViewSet)
-router.register(r'user_favorites', views.UserFavoriteViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    path('', include('lunch_and_learn_api.urls'))
 ]
