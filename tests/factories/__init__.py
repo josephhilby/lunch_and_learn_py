83ecedc437ca933ab9b66ebd31e8c47bafa251c0
Joseph Hilby <joseph.hilby@gmail.com>
2023-05-23 15:47:27 -0500

    Create factories

---
+++ tests/factories/__init__.py
@@ -0,0 +1,23 @@
+import factory
+from faker import Factory as FakerFactory
+
+from lunch_and_learn_api.models import User, Favorite, UserFavorite
+
+faker = FakerFactory.create()
+
+class UserFactory(factory.django.DjangoModelFactory):
+    name = factory.LazyAttribute(lambda x: faker.name())
+    email = factory.LazyAttribute(lambda x: faker.email())
+    password = factory.LazyAttribute(lambda x: faker.password())
+    api_key = factory.LazyAttribute(lambda x: faker.password())
+
+    class Meta:
+        model = User
+
+class FavoriteFactory(factory.django.DjangoModelFactory):
+    country = factory.LazyAttribute(lambda x: faker.country())
+    recipe_link = factory.LazyAttribute(lambda x: faker.url())
+    recipe_title = factory.LazyAttribute(lambda x: faker.sentence())
+
+    class Meta:
+        model = Favorite
